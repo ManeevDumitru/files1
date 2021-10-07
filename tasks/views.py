98f3937e10b56841edf549974ee532252b66657f
ManeevDumitru <m9h-MiQ-C4e-8JP>
2021-10-07 10:21:51 +0300

    Create action

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
 from django.core import serializers
 from tasks.models import Company
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
-    return HttpResponse(serializers.serialize('json',Company.objects.all()))
+    return HttpResponse(serializers.serialize('json', Company.objects.all()))
 
 
 def detail(request, question_id):
-    return HttpResponse(serializers.serialize('json', [ Company.objects.get(pk=question_id) ]))
+    return HttpResponse(serializers.serialize('json', [Company.objects.get(pk=question_id)]))
+
+
+@csrf_exempt
+def create(request):
+    print(request.POST)
+    company = Company(
+        name=request.POST.get("name"),
+        website=request.POST.get("website"),
+        totalEmployees=request.POST.get("totalEmployees"),
+    )
+    company.save()
+    return HttpResponse(status=201)
