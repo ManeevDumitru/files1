e7df3c3e824385fa08da0b7eb8b71a44a7cd3327
ManeevDumitru <m9h-MiQ-C4e-8JP>
2021-10-07 10:01:52 +0300

    Add actions

--- tasks/views.py
+++ tasks/views.py
@@ -5,3 +5,7 @@ from tasks.models import Company
 
 def index(request):
     return HttpResponse(serializers.serialize('json',Company.objects.all()))
+
+
+def detail(request, question_id):
+    return HttpResponse(serializers.serialize('json', [ Company.objects.get(pk=question_id) ]))
